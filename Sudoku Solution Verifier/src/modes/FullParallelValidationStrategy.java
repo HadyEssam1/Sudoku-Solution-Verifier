a0c92860da5f8460541d1f321ae544d43dd40072
yassin-124 <yassinelnobby@gmail.com>
2025-11-30 12:02:48 +0200

    modes

--- Sudoku Solution Verifier/src/modes/FullParallelValidationStrategy.java
+++ Sudoku Solution Verifier/src/modes/FullParallelValidationStrategy.java
@@ -1,10 +1,56 @@
 package modes;
 
+import checkers.Checker;
+import checkers.SingleBoxChecker;
+import checkers.SingleColumnChecker;
+import checkers.SingleRowChecker;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class FullParallelValidationStrategy implements ValidationStrategy {
     @Override
     public List<String> validate(int[][] grid) {
-        return List.of();
+        ArrayList<Thread> threads = new ArrayList<>();
+        ArrayList<Checker> checkers = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            SingleRowChecker src = new SingleRowChecker(grid, i);
+            checkers.add(src);
+            Thread t1 = new Thread(src);
+            threads.add(t1);
+        }
+        for (int i = 0; i < 9; i++) {
+            SingleColumnChecker scc = new SingleColumnChecker(grid, i);
+            checkers.add(scc);
+            Thread t1 = new Thread(scc);
+            threads.add(t1);
+        }
+        for (int i =0;i <= 6;i+=3) {
+            for (int j =0;j<= 6;j+= 3) {
+                SingleBoxChecker sbc = new SingleBoxChecker(grid, i, j);
+                checkers.add(sbc);
+                Thread t1 = new Thread(sbc);
+                threads.add(t1);
+            }
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        try {
+            for (Thread t : threads) {
+                    t.join();
+                }
+        } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return List.of("Validation interrupted: "+e.getMessage());
+                }
+
+        ArrayList<String> list=new ArrayList<>();
+        for(Checker c:checkers){
+            list.addAll(c.getErrors());
+        }
+            return List.of();
+        }
     }
-}
--- Sudoku Solution Verifier/src/modes/TripleThreadValidationStrategy.java
+++ Sudoku Solution Verifier/src/modes/TripleThreadValidationStrategy.java
@@ -1,10 +1,40 @@
 package modes;
 
+import checkers.BoxChecker;
+import checkers.ColumnChecker;
+import checkers.RowChecker;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TripleThreadValidationStrategy implements ValidationStrategy {
     @Override
     public List<String> validate(int[][] grid) {
+        RowChecker r=new RowChecker(grid);
+        ColumnChecker c=new ColumnChecker(grid);
+        BoxChecker b=new BoxChecker(grid);
+
+        Thread r1=new Thread(r);
+        Thread c1=new Thread(c);
+        Thread b1=new Thread(b);
+
+        r1.start();
+        c1.start();
+        b1.start();
+        try {
+            r1.join();
+            c1.join();
+            b1.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return List.of("Validation interrupted: "+ e.getMessage());
+        }
+
+        ArrayList<String> list=new ArrayList<>();
+        list.addAll(r.getErrors());
+        list.addAll(c.getErrors());
+        list.addAll(b.getErrors());
+
         return List.of();
     }
 }
