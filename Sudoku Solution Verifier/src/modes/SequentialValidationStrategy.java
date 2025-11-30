c8e3179ebae6be07b3212e006f4d2aad03917bc5
yassin-124 <yassinelnobby@gmail.com>
2025-11-30 11:06:58 +0200

    Update SequentialValidationStrategy.java

--- Sudoku Solution Verifier/src/modes/SequentialValidationStrategy.java
+++ Sudoku Solution Verifier/src/modes/SequentialValidationStrategy.java
@@ -1,11 +1,30 @@
 package modes;
 
+import checkers.BoxChecker;
+import checkers.ColumnChecker;
+import checkers.RowChecker;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SequentialValidationStrategy implements ValidationStrategy {
 
     @Override
     public List<String> validate(int[][] grid) {
+        RowChecker r=new RowChecker(grid);
+        ColumnChecker c=new ColumnChecker(grid);
+        BoxChecker b=new BoxChecker(grid);
+
+        r.run();
+        c.run();
+        b.run();
+
+        ArrayList<String> list=new ArrayList<>();
+        list.addAll(r.getErrors());
+        list.addAll(c.getErrors());
+        list.addAll(b.getErrors());
+
+
         return List.of();
     }
 }
