c1920de9a1312d308c7b279f1e4c137c4d206cf5
yassin-124 <yassinelnobby@gmail.com>
2025-11-30 12:05:59 +0200

    Update TripleThreadValidationStrategy.java

--- Sudoku Solution Verifier/src/modes/TripleThreadValidationStrategy.java
+++ Sudoku Solution Verifier/src/modes/TripleThreadValidationStrategy.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class TripleThreadValidationStrategy implements ValidationStrategy {
     @Override
     public List<String> validate(int[][] grid) {
-        return List.of();
+        return list;
     }
 }
