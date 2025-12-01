cbd02952340de174b473c4f9e094cbb49abb3ead
HadyEssam1 <hadyessam731@gmail.com>
2025-12-01 03:51:05 +0200

    .

--- Sudoku Solution Verifier/src/checkers/SingleBoxChecker.java
+++ Sudoku Solution Verifier/src/checkers/SingleBoxChecker.java
@@ -27,46 +27,10 @@ public class SingleBoxChecker extends Checker {
         }
         for (int i=1 ; i<=9 ; i++){
             if (count[i]>1){
-                addError("Box "+(box+1)+", #"+i+", "+ Arrays.toString(getBoxArray(startRow,startColumn,i)));
+                addError("BOX " + (box + 1) + ", #" + i + ", " + Arrays.toString(getBoxArray(startRow, startColumn, i)));
             }
         }
     }
-//    public int[][] getBoxArray(int startRow, int startColumn){
-//        int[][] box =new int[3][3];
-//        for (int row = 0; row <3 ; row++){
-//            System.arraycopy(grid[startRow+row] , startColumn , box[row], 0,3 );
-//        }
-//        return box;
-//    }
-//    public int[][] getBoxArray(int startRow, int startColumn, int duplicatedValue) {
-//
-//        int[][] positions = new int[9][2];
-//        int index = 0;
-//
-//        for (int row = startRow; row < startRow + 3; row++) {
-//            for (int column = startColumn; column < startColumn + 3; column++) {
-//                if (grid[row][column] == duplicatedValue) {
-//                    positions[index][0] = row + 1;
-//                    positions[index][1] = column + 1;
-//                    index++;
-//                }
-//            }
-//        }
-//        return java.util.Arrays.copyOf(positions, index);
-//    }
-//    public int[] getBoxArray(int startRow, int startColumn, int duplicatedValue) {
-//
-//        int[] positions = new int[9];
-//        int index = 0;
-//
-//        for (int row = startRow; row < startRow + 3; row++) {
-//            for (int column = startColumn; column < startColumn + 3; column++) {
-//                positions[index] = grid[row][column];
-//                index++;
-//            }
-//        }
-//        return positions;
-//    }
 
     public int[] getBoxArray(int startRow, int startColumn, int duplicatedValue) {
         int original = -1;
